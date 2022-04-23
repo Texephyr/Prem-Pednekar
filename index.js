3c8b9d5918951adf16fbbf9c57e8ec3115913196
AbhishekPanat <77336031+AbhishekPanat@users.noreply.github.com>
2022-04-23 14:16:22 +0530

    TEST

---
+++ index.js
@@ -0,0 +1,20 @@
+var i=3
+
+function change1()
+{
+    if(i==9)
+    {
+        i=2;
+    }
+    document.getElementById('first').src='images/main'+i+'.jpeg'
+    i++;
+}
+function change2()
+{
+    if(i==9)
+    {
+        i=2;
+    }
+document.getElementById('second').src='images/main'+i+'.jpeg'
+i++;
+}
